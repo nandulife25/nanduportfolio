f46596affd2d4cc4e72ade296c534df69e19cdf3
nandulife25 <68550033+nandulife25@users.noreply.github.com>
2020-07-20 21:31:11 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -117,7 +117,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-#STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
